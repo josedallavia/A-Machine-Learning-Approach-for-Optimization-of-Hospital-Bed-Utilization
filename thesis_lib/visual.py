176396b673eac36d9b0498816f0e69832cb5a113
Josefina Dalla Via <josefinadallavia@Josefinas-MacBook-Pro-2.local>
2020-02-01 15:44:03 -0300

    Plot functions added on thesis_lib

---
+++ thesis_lib/visual.py
@@ -0,0 +1,25 @@
+import pandas as df
+
+def plot_top_categories_mean(df,categorical,n,to_plot,figsize=(20,10),title=None):
+    
+    categories_top = df.groupby([categorical]).nunique().sort_values(by='Nro Adm', 
+                                                            ascending=False).head(n)[categorical]
+    
+    
+    df[df[categorical].isin(categories_top.axes[0])].groupby(categorical).mean()[to_plot].plot.bar(figsize=figsize,
+                                                                                                  title=title)
+
+    
+def plot_top_categories_count(df,categorical,n,figsize=(20,10), title=None):
+    
+    categories_top = df.groupby([categorical]).nunique().sort_values(by='Nro Adm', 
+                                                            ascending=False).head(n)[categorical]
+    
+    
+    df[df[categorical].isin(categories_top.axes[0])].groupby(categorical).count()['Nro Adm'].plot.bar(figsize=figsize,
+                                                                                                      title=title)
+
+    
+    
+
+    
