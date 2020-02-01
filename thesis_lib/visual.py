4c65d846c1f9f44350404142ed4e3589d25a681c
Josefina Dalla Via <josefinadallavia@Josefinas-MacBook-Pro-2.local>
2020-02-01 19:29:55 -0300

    Added function for combined plot

--- thesis_lib/visual.py
+++ thesis_lib/visual.py
@@ -6,6 +6,8 @@ def plot_top_categories_mean(df,categorical,n,to_plot,figsize=(20,10),title=None
                                                             ascending=False).head(n)[categorical]
     
     
+  
+    
     df[df[categorical].isin(categories_top.axes[0])].groupby(categorical).mean()[to_plot].plot.bar(figsize=figsize,
                                                                                                   title=title)
 
@@ -19,6 +21,16 @@ def plot_top_categories_count(df,categorical,n,figsize=(20,10), title=None):
     df[df[categorical].isin(categories_top.axes[0])].groupby(categorical).count()['Nro Adm'].plot.bar(figsize=figsize,
                                                                                                       title=title)
 
+def plot_top_categories(df,categorical,n,figsize=(20,10),title=None):
+    
+    categories_top = df.groupby([categorical]).nunique().sort_values(by='Nro Adm', 
+                                                            ascending=False).head(n)[categorical]
+    
+
+    df[df[categorical].isin(categories_top.axes[0])].groupby(categorical).agg({'Nro Adm': 'count',
+                                                                               'Length': 'mean'}).plot.bar(figsize=figsize,
+                                                                                                  title=title,
+                                                                                                subplots=True,sharex=True)
     
     
 
