27bd84b46c23410b9fdbebb0390473e6a819d7cd
Josefina Dalla Via <josefinadallavia@gmail.com>
2020-06-04 21:41:55 -0300

    refactor

--- thesis_lib/visual.py
+++ thesis_lib/visual.py
@@ -9,82 +9,52 @@ def plot_top_categories_mean(df,
                             title=None):
     
     categories_top = df.groupby(
-                                [categorical]
-                                ).nunique().sort_values(by='admission_id', 
-                                                        ascending=False).head(n)[categorical]
+                    [categorical]).nunique().sort_values(
+                    by='admission_id',ascending=False).head(n)[categorical]
                                                         
     df[df[categorical].isin(
-                        categories_top.axes[0]
-                            )].groupby(
-                                    categorical).mean()[
-                                        to_plot
-                                    ].sort_values(ascending=False).plot.bar(figsize=figsize,
-                                                                            title=title,
-                                                                            color='blue')
+                    categories_top.axes[0])].groupby(
+                    categorical).mean()[to_plot].sort_values(
+                    ascending=False).plot.bar(figsize=figsize,title=title,color='blue')
 
     
 def plot_top_categories_count(df,categorical,n,figsize=(20,10), title=None):
     
     categories_top = df.groupby(
-                                [categorical]
-                                ).nunique().sort_values(by='admission_id', 
-                                                        ascending=False).head(n)[categorical]                                               
+                        [categorical]).nunique().sort_values(
+                        by='admission_id',ascending=False).head(n)[categorical]  
+                                                                     
     df[df[categorical].isin(
-                        categories_top.axes[0]
-                        )].groupby(
-                            categorical
-                            ).count()[
-                                'admission_id'
-                                    ].sort_values(ascending=False).plot.bar(figsize=figsize,
-                                                                            title=title, 
-                                                                            color='blue')
+                        categories_top.axes[0])].groupby(
+                        categorical).count()['admission_id'
+                        ].sort_values(ascending=False).plot.bar(
+                        figsize=figsize,title=title,color='blue')
 
 def plot_top_categories(df,categorical,n,figsize=(20,10),title=None):
     
-    categories_top = df.groupby(
-                            [categorical]
-                            ).nunique().sort_values(by='admission_id',
-                                                    ascending=False).head(n)[categorical]
-    df[df[
-        categorical
-            ].isin(
-                categories_top.axes[0]
-                    )].groupby(
-                            categorical
-                            ).agg({
-                                'admission_id': 'count',
-                                'length': 'mean'
-                                }).sort_values(
-                                        by='admission_id',
-                                        ascending=False
-                                        ).plot.bar(
-                                            figsize=figsize,
-                                            title=['# of admissions by '+categorical,
-                                            'Avg. hospitalization length (hours) by '+categorical],
-                                            subplots=True,
-                                            sharex=True,
-                                            legend=False
-                                            )
+    categories_top = df.groupby([categorical]).nunique().sort_values(
+                        by='admission_id',ascending=False).head(n)[categorical]
+    
+    df[df[categorical].isin(categories_top.axes[0])].groupby(categorical).agg({
+                'admission_id': 'count',
+                'length': 'mean'
+                }).sort_values(by='admission_id',ascending=False).plot.bar(
+                figsize=figsize,
+                title=['# of admissions by '+categorical,
+                'Avg. hospitalization length (hours) by '+categorical],
+                subplots=True,sharex=True,legend=False)
                                             
-def plot_scatter_length_vs_count(df,
-                                 dimension,
-                                 title=None,
-                                 labeled=False,
-                                 xlim=None,
-                                 ylim=None,
-                                 figsize=(15,8),
-                                 N_rows=500):
+def plot_scatter_length_vs_count(df,dimension,title=None,labeled=False,
+                                 xlim=None,ylim=None,figsize=(15,8),N_rows=500):
     
     f, ax = plt.subplots(figsize=figsize)
    
     tmp = df.groupby(dimension).aggregate({
-                                    'admission_id': 'count', 
-                                    'length': 'mean'
-                                    }).sort_values(
-                                                by='admission_id',
-                                                axis=0,
-                                                ascending=False
-                                                ).head(N_rows)
+                'admission_id': 'count', 
+                'length': 'mean'}).sort_values(
+                by='admission_id',
+                axis=0,
+                ascending=False).head(N_rows)
     
     x = tmp['admission_id']
     y= tmp['length']
