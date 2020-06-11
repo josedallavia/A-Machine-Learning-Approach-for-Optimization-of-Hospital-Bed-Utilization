dc7eb0184abbe812446a2f8e9a49c56bbe2c8fc5
Josefina Dalla Via <josefinadallavia@gmail.com>
2020-06-11 20:03:01 -0300

    implemented rf with lightGBM

--- thesis_lib/modelling/classifiers.py
+++ thesis_lib/modelling/classifiers.py
@@ -7,7 +7,7 @@ from sklearn.metrics import roc_auc_score
 
 class LGBM_classifier(LGBMClassifier):
     def __init__(self,objective='binary',metric='auc',is_unbalance=True,max_depth=7, learning_rate=0.1,
-                 num_iterations=100,feature_names='auto'):
+                 num_iterations=100,feature_names='auto', boosting='gbdt',bagging_freq=0,bagging_fraction=1.0):
         self.objective = objective
         self.metric = metric
         self.is_unbalance = is_unbalance
@@ -15,6 +15,9 @@ class LGBM_classifier(LGBMClassifier):
         self.learning_rate = learning_rate
         self.num_iterations = num_iterations
         self.feature_names= feature_names
+        self.boosting = boosting
+        self.bagging_freq = bagging_freq
+        self.bagging_fraction = bagging_fraction
 
     def get_params(self,deep=True):
         return {'objective': self.objective,
@@ -23,12 +26,16 @@ class LGBM_classifier(LGBMClassifier):
                 'max_depth' : self.max_depth,
                 'learning_rate': self.learning_rate,
                 'num_iterations': self.num_iterations,
-                'feature_names': self.feature_names}
+                'feature_names': self.feature_names,
+                'boosting': self.boosting,
+                'bagging_freq': self.bagging_freq,
+                'bagging_fraction': self.bagging_fraction}
 
     def set_params(self, **parameters):
         for parameter, value in parameters.items():
             setattr(self, parameter, value)
         return self
+
     def fit(self, X_train,y_train,X_val=None,y_val=None):
 
         # to record eval results for plotting
