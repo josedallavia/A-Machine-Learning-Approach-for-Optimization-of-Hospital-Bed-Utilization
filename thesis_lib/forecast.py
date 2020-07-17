f8b1f1aff12455eaede1b486567dbe08b28642e1
Josefina Dalla Via <josefinadallavia@gmail.com>
2020-07-17 01:23:21 -0300

    prompt message if there is no data for the date

--- thesis_lib/forecast.py
+++ thesis_lib/forecast.py
@@ -28,34 +28,37 @@ def forecast_discharges(date):
 
     date_data = data.test.X[data.test.X['date'] == date]
 
-    assert len(date_data) > 0, 'No data available for the specified date'
-    print('Forecasting discharges for date: {year}-{month}-{day}'.format(year=date.year,
-                                                                         month=date.month,
-                                                                         day=date.day))
+    if len(date_data) == 0:
+        print('There is no data available for the specified date!')
+    else:
+        
+        print('Forecasting discharges for date: {year}-{month}-{day}'.format(year=date.year,
+                                                                             month=date.month,
+                                                                             day=date.day))
 
-    # Load model
-    with open("experiments/optimized_gdbt_model.pkl", "rb") as input_file:
-        lgbm_model = pkl.load(input_file)
+        # Load model
+        with open("experiments/optimized_gdbt_model.pkl", "rb") as input_file:
+            lgbm_model = pkl.load(input_file)
 
-    # transformed data and get the predictions
-    transformed_data = lgbm_model.pipeline.transform(date_data)
-    predictions = lgbm_model.predict(transformed_data)
+        # transformed data and get the predictions
+        transformed_data = lgbm_model.pipeline.transform(date_data)
+        predictions = lgbm_model.predict(transformed_data)
 
-    # Load confidence thresholds to use:
-    with open('confidence_dict.json', 'r') as json_file:
-        confidence_thresholds = json.load(json_file)
+        # Load confidence thresholds to use:
+        with open('confidence_dict.json', 'r') as json_file:
+            confidence_thresholds = json.load(json_file)
 
-    # Compute forecasts:
-    for confidence_level in confidence_thresholds:
-        threshold = confidence_thresholds[confidence_level]
-        forecast = sum([1 for prob in predictions if prob > threshold])
+        # Compute forecasts:
+        for confidence_level in confidence_thresholds:
+            threshold = confidence_thresholds[confidence_level]
+            forecast = sum([1 for prob in predictions if prob > threshold])
 
-        print(('With {p} % of confidence, number of discharges will be at least {N}'
-               .format(p=confidence_level, N=forecast)))
+            print(('With {p} % of confidence, number of discharges will be at least {N}'
+                   .format(p=confidence_level, N=forecast)))
 
-        output['confidence %'].append(confidence_level)
-        output['lower bound forecast'].append(forecast)
+            output['confidence %'].append(confidence_level)
+            output['lower bound forecast'].append(forecast)
 
-    return pd.DataFrame(output)
+        return pd.DataFrame(output)
 
 
